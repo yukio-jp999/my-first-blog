0021a1a19577e06c062f5b26a8483b123b4f9826
yukio <602344@gmail.com>
2020-04-14 15:46:36 +0900

    Changing ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'yukio.pythonanywhere.com']
